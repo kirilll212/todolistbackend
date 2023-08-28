eb4702e33793a5e1f078749b8ed0c4b385331487
kirilll212 <kushkiril2005@gmail.com>
2023-08-28 15:32:06 +0300

    fixede token problem

--- App/controllers/usersController.js
+++ App/controllers/usersController.js
@@ -4,25 +4,25 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 class userController {
-    async register(req, res){
+    async register(req, res) {
         try {
             const { email, password } = req.body;
             const hashedPassword = await bcrypt.hash(password, 10);
-    
+
             const user = await User.create({
                 email: email,
                 password: hashedPassword
             });
 
-            const token = jwt.sign({ userId: user.id}, config.jwtSecret, {expiresIn: '1h'})
-    
+            const token = jwt.sign({ userId: user.id }, config.jwtSecret, { expiresIn: '1h' })
+
             res.status(201).json({ message: 'User registered successfully', token: token });
-        } 
+        }
         catch (err) {
             res.status(500).json({ error: err.message });
         }
     }
-    
+
     async login(req, res) {
         try {
             const { email, password } = req.body;
@@ -38,7 +38,10 @@ class userController {
             if (!passwordMatch) {
                 return res.status(401).json({ message: 'Invalid credentials' });
             }
-            res.json({ message: 'User logged in successfully' });
+
+            const token = jwt.sign({ userId: user.id }, config.jwtSecret, { expiresIn: '1h' });
+
+            res.json({ message: 'User logged in successfully', token: token });
         } catch (err) {
             res.status(500).json({ error: err.message });
         }
