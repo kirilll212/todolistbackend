9d5b033adfe8d90744a25e533141ff1c2a3a9383
kirilll212 <kushkiril2005@gmail.com>
2023-09-04 16:44:25 +0300

    adding swagger docs

--- App/index.js
+++ App/index.js
@@ -3,12 +3,29 @@ const sequelize = require('./util/database')
 const userRoute = require('./routes/users')
 const todoRoute = require('./routes/todos')
 const authMiddleware = require('./controllers/middleware/authMiddleware')
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const Router = express.Router;
 const router = new Router();
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json());
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: '1.0.0',
+        title: 'Todo API',
+        description: 'API for managing todo items.',
+      },
+    },
+    apis: [__dirname + '/util/swagger.yaml'],
+  };
+  
+  const swaggerSpec = swaggerJsdoc(swaggerOptions);
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/users', userRoute(router))
 app.use('/todos', authMiddleware, todoRoute(router))
