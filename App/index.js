bb26463c84623d35a57e2c7f54124392551b3d36
kirilll212 <kushkiril2005@gmail.com>
2023-09-14 17:00:13 +0300

    adding admin panel layout

--- App/index.js
+++ App/index.js
@@ -12,6 +12,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json());
+app.use('/admin', express.static(__dirname + '/public/admin'))
 
 const swaggerOptions = {
   swaggerDefinition: {
